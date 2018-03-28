933de46312668d6ca29d384f73021c536f6410a7
Brendon Carrasquillo <brendon.wanderlust@gmail.com>
2018-03-27 20:02:17 -0400

    First commit post clone

--- exercises/level1.js
+++ exercises/level1.js
@@ -1,34 +1,46 @@
 function lvl1exercise1() {
 	// Declare a variable without instantiating it and return it!
+  var brendon;
+  return brendon;
 
 }
 
 function lvl1exercise2() {
 	// Declare and instantiate a number and return it
-
+  var num1 = 1;
+  return num1;
 }
 
 function lvl1exercise3() {
 	// Declare and instantiate a floating point number that isn't a whole number and return it
-
+  var float1 = 1.2;
+  return float1
 }
 
 function lvl1exercise4() {
 	// Declare and instantiate a string "Hello World!" and return it
-
+  var string1 = "Hello World!";
+  return string1;
 }
 
 function lvl1exercise5() {
 	// Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
-
+  var array1 = ["Hello World!", 4];
+  return array1;
 }
 
 function lvl1exercise6() {
 	// Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
-
+  var object1 = {
+    "Hello World": 4,
+  }
+  return object1;
 }
 
 function lvl1exercise7() {
 	// Declare and instantiate a boolean value 'false' and return it
-
+  function itsBrendon() {
+    var nope = false;
+  }
+  return false;
 }
